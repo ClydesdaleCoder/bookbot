92e70fd299e39107a86f0911126f9db8e5506adf
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-02-27 21:06:56 -0800

    Report with ARGgit add main.py

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from stats import get_book_text,word_count,get_letter_count,sort_count_by_char
-
+import sys
+argument= sys.argv
+path=sys.argv[1]
 def main(): 
-    file_contents = get_book_text("books/frankenstein.txt")
-    num_words = word_count(file_contents)
-    letter_count = get_letter_count(file_contents) 
-    sorted_count = sort_count_by_char(letter_count)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt ")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
+    if len(argument)  != 2:
+        print("Usage: python3 main.py <path_to_book> ")
+        sys.exit(1)
+    else:
 
-    for item in sorted_count: 
-        char = list(item.keys())[0]
-        count = item[char]
-        if not char.isalpha():
-             continue
-        print(f"{char}:{count}")
+        file_contents = get_book_text(path)
+        num_words = word_count(file_contents)
+        letter_count = get_letter_count(file_contents) 
+        sorted_count = sort_count_by_char(letter_count)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path} ")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for item in sorted_count: 
+            char = list(item.keys())[0]
+            count = item[char]
+            if not char.isalpha():
+                continue 
+            print(f"{char}:{count}")
 
-    print("============= END ===============")
+        print("============= END ===============")
             
 main()
 
